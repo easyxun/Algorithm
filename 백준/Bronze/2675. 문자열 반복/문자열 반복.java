57a4bec9c23dd32ebdd6867a587a195730b997cd
easyxun <107982536+easyxun@users.noreply.github.com>
2024-03-27 23:18:26 +0900

    [Bronze II] Title: 문자열 반복, Time: 116 ms, Memory: 14080 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.java
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.java
@@ -5,24 +5,20 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int count = Integer.parseInt(br.readLine());
 
-        int n = Integer.parseInt(br.readLine());
-
-        for (int i = 0; i < n; i++) {
-
+        for (int i = 0; i < count; i++) {
             String[] arr = br.readLine().split(" ");
+            int repeat = Integer.parseInt(arr[0]);
+            String word = arr[1];
 
-            int R = Integer.parseInt(arr[0]);
-            String s = arr[1];
+            StringBuilder sb = new StringBuilder();
 
-            for (int j = 0; j < s.length(); j++) {
-                for (int k = 0; k < R; k++) {
-                    System.out.print(s.charAt(j));
-                }
+            for (int j = 0; j < word.length(); j++) {
+                sb.append(word.substring(j, j + 1).repeat(repeat));
             }
-            System.out.println();
+
+            System.out.println(sb);
         }
     }
-
-
 }
