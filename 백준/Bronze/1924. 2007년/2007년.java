fa5e3d75fe988bc26edb2e1e413ff421f1f707c5
easyxun <107982536+easyxun@users.noreply.github.com>
2024-03-27 16:39:28 +0900

    [Bronze I] Title: 2007년, Time: 296 ms, Memory: 19192 KB -BaekjoonHub

--- 백준/Bronze/1924. 2007년/2007년.java
+++ 백준/Bronze/1924. 2007년/2007년.java
@@ -6,8 +6,35 @@ public class Main {
         int mon = sc.nextInt();
         int day = sc.nextInt();
         Calendar cal = Calendar.getInstance();
-        cal.clear();
         cal.set(2007, mon - 1, day);
-        System.out.println(cal.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.SHORT, Locale.US).toUpperCase());
+        
+        int yoil = cal.get(Calendar.DAY_OF_WEEK);
+        
+        switch (yoil) {
+            case Calendar.SUNDAY:
+                System.out.println("SUN");
+                break;
+            case Calendar.MONDAY:
+                System.out.println("MON");
+                break;
+            case Calendar.TUESDAY:
+                System.out.println("TUE");
+                break;
+            case Calendar.WEDNESDAY:
+                System.out.println("WED");
+                break;
+            case Calendar.THURSDAY:
+                System.out.println("THU");
+                break;
+            case Calendar.FRIDAY:
+                System.out.println("FRI");
+                break;
+            case Calendar.SATURDAY:
+                System.out.println("SAT");
+                break;
+            default:
+                System.out.println("Invalid day");
+                break;
+        }
     }
 }
