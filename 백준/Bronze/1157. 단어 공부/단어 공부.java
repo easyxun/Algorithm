d2081ecc6caf2b7e6aaeab07a603c8731f138c43
easyxun <107982536+easyxun@users.noreply.github.com>
2024-03-28 16:29:26 +0900

    [Bronze I] Title: 단어 공부, Time: 412 ms, Memory: 38892 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -6,36 +6,46 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        String str = br.readLine().toUpperCase(); // 입력된 문자를 대문자로 변환하여 저장
-        char[] arr = str.toCharArray(); // 문자 배열로 저장
+        // 입력된 문자열을 대문자로 변환하여 저장
+        String str = br.readLine().toUpperCase();
+        // 문자열을 문자 배열로 저장
+        char[] arr = str.toCharArray();
 
-        HashMap<Character, Integer> map = new HashMap<>(); // 출현빈도 저장
-        HashSet<Character> set = new HashSet<>(); // 중복 문자 제거
+        // 출현 빈도를 저장할 HashMap
+        HashMap<Character, Integer> map = new HashMap<>();
+        // 중복 문자를 제거하기 위한 HashSet
+        HashSet<Character> set = new HashSet<>();
 
+        // 문자 배열을 순회하면서 출현 빈도를 계산하고 중복 문자를 제거
         for (char c : arr) {
-            if (set.contains(c)) { // Hashset에 문자가 있으면
-                map.put(c, map.get(c) + 1); // (문자,출현빈도 +1)
-            } else {
-                map.put(c, 1); // 문자가 없으면 초기화
-                set.add(c); // 중복문자 확인, Hashset에 추가
+            if (set.contains(c)) { // HashSet에 문자가 이미 있는 경우
+                map.put(c, map.get(c) + 1); // 해당 문자의 빈도를 1 증가
+            } else { // HashSet에 문자가 없는 경우
+                map.put(c, 1); // 해당 문자의 빈도를 1로 초기화
+                set.add(c); // HashSet에 문자 추가
             }
         }
 
-        char maxChar = ' '; // 가장 많이 출현한 알파벳을 저장할 변수 초기화
-        int max = 0; // 알파벳의 최대 출현 빈도를 저장할 변수 초기화
+        // 가장 많이 출현한 알파벳과 그 빈도를 저장할 변수 초기화
+        char maxChar = ' ';
+        int max = 0;
 
+        // HashMap을 순회하면서 가장 많이 출현한 알파벳과 그 빈도를 찾음
         for (char c : map.keySet()) {
-            int value = map.get(c);
-            if (value > max) {
-                maxChar = c;
-                max = value;
-            } else if (value == max) {
-                maxChar = '?';
+            int i = map.get(c); // 현재 문자의 출현 빈도
+            if (i > max) { // 최대 출현 빈도를 갱신할 경우
+                maxChar = c; // 가장 많이 출현한 알파벳을 업데이트
+                max = i; // 최대 출현 빈도를 업데이트
+            } else if (i == max) { // 최대 출현 빈도와 동일한 경우
+                maxChar = '?'; // 다수의 알파벳이 최대 출현 빈도를 가지고 있으므로 '?'로 설정
             }
         }
 
-        bw.write(maxChar); // 결과 출력
-        bw.flush(); // 출력 버퍼 비우기
-        bw.close(); // 출력 스트림 닫기
+        // 결과를 출력 스트림에 쓰기
+        bw.write(maxChar);
+        // 출력 버퍼 비우기
+        bw.flush();
+        // 출력 스트림 닫기
+        bw.close();
     }
 }
