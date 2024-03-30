4d6413e3c9c04e52efea9806b4aa42b9a2275b04
easyxun <107982536+easyxun@users.noreply.github.com>
2024-03-30 15:52:15 +0900

    [Bronze II] Title: 진법 변환, Time: 128 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Bronze/2745. 진법 변환/진법 변환.java
+++ 백준/Bronze/2745. 진법 변환/진법 변환.java
@@ -6,31 +6,28 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        // 입력 받기
         StringTokenizer st = new StringTokenizer(br.readLine());
-        String n = st.nextToken(); // B진법 수
-        int a = Integer.parseInt(st.nextToken()); // B진법
+        String n = st.nextToken(); // n진법 갯수
 
-        int num = 0; // 10진법으로 변환된 수를 저장할 변수
+        int a = Integer.parseInt(st.nextToken()); // n진법
+        int num = 0; // 10진법
 
-        // 문자열의 각 자리에 대해 진법 변환 수행
         for (int i = 0; i < n.length(); i++) {
             char c = n.charAt(i);
-
-            int temp; // 현재 자리의 숫자를 저장할 변수
+            int temp; // i번째 수를 10진법으로 변환하여 저장할 변수
             if (c >= 'A') {
-                temp = c - 'A' + 10; // A ~ Z는 10부터 순서대로 저장되므로, 'A'를 빼고 10을 더함
+                temp = c - 55; // A ~ Z는 10부터 순서대로 저장 -> -65('A')+10
             } else {
-                temp = c - '0'; // 문자 '0'의 ASCII 코드 값인 48을 빼줌으로써 숫자를 얻음
+                temp = c - 48; // 0의 아스키코드 48 -> -48
             }
 
-            num = num * a + temp; // 진법 변환 수행
+            num = num * a + temp;
         }
 
-        // 결과 출력
         bw.write(String.valueOf(num));
         bw.flush();
         bw.close();
         br.close();
+
     }
 }
