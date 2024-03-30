40727f888e16be4a882c1d696e25c1a41fef0448
easyxun <107982536+easyxun@users.noreply.github.com>
2024-03-30 19:19:32 +0900

    [Silver III] Title: 팰린드롬 만들기, Time: 128 ms, Memory: 14288 KB -BaekjoonHub

--- 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.java
+++ 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.java
@@ -1,62 +1,52 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String word = br.readLine();
 
-        // 각 알파벳의 빈도수를 저장하는 HashMap
-        Map<Character, Integer> frequencyMap = new HashMap<>();
-
-        // 각 알파벳의 빈도수 계산
-        for (char ch : word.toCharArray()) {
-            frequencyMap.put(ch, frequencyMap.getOrDefault(ch, 0) + 1);
+        Map<Character, Integer> m = new HashMap<>(); // 알파벳 총 갯수
+        for (char c : word.toCharArray()) {
+            m.put(c, m.getOrDefault(c, 0) + 1);
         }
 
-        // 홀수 빈도수를 가진 알파벳의 개수 계산
-        int oddCount = 0;
-        for (int count : frequencyMap.values()) {
-            if (count % 2 != 0) {
-                oddCount++;
+        // 알파벳이 홀수 일 경우 판별
+        int i = 0; // 알파벳 하나의 빈도수
+        for (int frequency : m.values()) {
+            if (frequency % 2 != 0) {
+                i++;
             }
         }
-
-        // 홀수 빈도수를 가진 알파벳이 1개를 초과하면 팰린드롬을 만들 수 없음
-        if (oddCount > 1) {
+        
+        // i가 1이상이면 팰린드롬 만들 수 없음
+        if (i > 1) {
             System.out.println("I'm Sorry Hansoo");
         } else {
-            // 팰린드롬을 만들기 위한 문자열 생성
-            StringBuilder palindrome = new StringBuilder();
+            StringBuilder arr1 = new StringBuilder();
 
-            // 팰린드롬의 앞부분 구성
-            for (char ch = 'A'; ch <= 'Z'; ch++) {
-                int count = frequencyMap.getOrDefault(ch, 0);
-                if (count > 0) {
-                    // 앞쪽에 절반을 삽입
-                    for (int i = 0; i < count / 2; i++) {
-                        palindrome.append(ch);
+            // 팰린드롬 앞부분
+            for (char c = 'A'; c <= 'Z'; c++) {
+                int frequency = m.getOrDefault(c, 0);
+                if (frequency > 0) {
+                    for (int j = 0; j < frequency / 2; j++) {
+                        arr1.append(c);
                     }
                 }
             }
 
-            // 뒷부분 구성 (앞부분을 뒤집어서 사용)
-            StringBuilder reversePalindrome = new StringBuilder(palindrome).reverse();
+            // 팰린드롬 뒷부분
+            StringBuilder arr2 = new StringBuilder(arr1).reverse();
 
-            // 홀수 빈도수를 가진 알파벳이 있는 경우 중앙에 삽입
-            for (char ch = 'A'; ch <= 'Z'; ch++) {
-                int count = frequencyMap.getOrDefault(ch, 0);
-                if (count % 2 != 0) {
-                    palindrome.append(ch);
+            // i를 중앙에 삽입
+            for (char c = 'A'; c <= 'Z'; c++) {
+                int frequency = m.getOrDefault(c, 0);
+                if (frequency % 2 != 0) {
+                    arr1.append(c);
                     break;
                 }
             }
-
-            // 결과 출력
-            System.out.println(palindrome.toString() + reversePalindrome.toString());
+            System.out.println(arr1.toString() + arr2.toString());
         }
     }
 }
