4e6a56ddaa1d879e92ecf4db982aea6f9959d91c
easyxun <107982536+easyxun@users.noreply.github.com>
2024-03-30 11:56:42 +0900

    [Bronze II] Title: KMP는 왜 KMP일까?, Time: 124 ms, Memory: 14156 KB -BaekjoonHub

---
+++ 백준/Bronze/2902. KMP는 왜 KMP일까？/KMP는 왜 KMP일까？.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch >= 'A' && ch <= 'Z') {
+                sb.append(ch);
+            }
+        }
+        
+        System.out.println(sb.toString());
+    }
+}
