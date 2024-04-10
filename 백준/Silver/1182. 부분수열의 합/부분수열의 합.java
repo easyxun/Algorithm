b09bdade947737955ac409a6707c4caad8639abe
easyxun <107982536+easyxun@users.noreply.github.com>
2024-04-10 18:55:47 +0900

    [Silver II] Title: 부분수열의 합, Time: 132 ms, Memory: 14128 KB -BaekjoonHub

---
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken()); // 배열 크기
+        int s = Integer.parseInt(st.nextToken()); // 부분 수열의 합
+
+        st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = Integer.parseInt(st.nextToken()); // 수열
+        }
+
+        int count = dfs(arr, 0, 0, s);
+
+        // 부분 수열의 합이 0이면 count에서 1을 빼고 출력
+        if (s == 0) {
+            System.out.println(count - 1);
+        } else {
+            System.out.println(count);
+        }
+    }
+
+    static int dfs(int[] arr, int index, int sum, int check){
+        // 종료 조건: 현재 인덱스가 배열의 크기와 같을 때
+        if(index == arr.length) {
+            // 현재까지의 합이 target과 같으면 경우의 수 1 반환, 아니면 0 반환
+            return sum == check ? 1 : 0;
+        }
+
+        // 현재 인덱스를 포함하는 경우와 포함하지 않는 경우에 대해 재귀 호출하여 결과를 합산
+        int in = dfs(arr, index+1, sum + arr[index], check); // 현재 인덱스를 포함하는 경우
+        int ex = dfs(arr, index+1, sum, check); // 현재 인덱스를 포함하지 않는 경우
+
+        // 현재 인덱스를 포함하는 경우와 포함하지 않는 경우의 결과를 합산하여 반환
+        return in + ex;
+    }
+}
