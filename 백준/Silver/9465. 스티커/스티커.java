fa70c6e205e85fa14dbe0add0ad4803b2ec1a302
easyxun <107982536+easyxun@users.noreply.github.com>
2024-04-16 11:50:37 +0900

    [Silver I] Title: 스티커, Time: 612 ms, Memory: 130684 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.java
+++ 백준/Silver/9465. 스티커/스티커.java
@@ -6,26 +6,26 @@ public class Main {
         int T = Integer.parseInt(br.readLine());
 
         for (int i = 0; i < T; i++) {
-            int N = Integer.parseInt(br.readLine());
             
+            int N = Integer.parseInt(br.readLine());
             int[][] arr = new int[2][N + 1];
             int[][] dp = new int[2][N + 1];
             for (int j = 0; j < 2; j++) {
-                String[] inputs = br.readLine().split(" ");
+                String[] s = br.readLine().split(" ");
                 for (int k = 1; k <= N; k++) {
-                    arr[j][k] = Integer.parseInt(inputs[k - 1]);
+                    arr[j][k] = Integer.parseInt(s[k - 1]);
                 }
             }
-            
+
             dp[0][1] = arr[0][1];
             dp[1][1] = arr[1][1];
-
             for (int j = 2; j <= N; j++) {
                 dp[0][j] = Math.max(dp[1][j - 1], dp[1][j - 2]) + arr[0][j];
                 dp[1][j] = Math.max(dp[0][j - 1], dp[0][j - 2]) + arr[1][j];
             }
 
             System.out.println(Math.max(dp[0][N], dp[1][N]));
+            
         }
     }
 }
